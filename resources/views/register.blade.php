d2add68ef6e7ff7fd2f5848a9fd0fc2b6529310e
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-02-11 20:27:07 +0100

    added form for registration page and styling in css file and form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,9 +1,69 @@
 <x-layout>
-    <section class="">
-        <main class="">
+
+    <section class="greybg">
+        <div class="pt-5 pb-5">
+            <div class="container-xxl">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                        <div class="card" style="border-radius: 15px;">
+                            <div class="card-body p-5">
+                                <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+
+                                <form method="POST" action="/register">
+                                @csrf
+                                    <div class="form-outline mb-4">
+                                        <input type="text" class="form-control form-control-lg" />
+                                        <label class="form-label" for="username">Your Username</label>
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="email" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example3cg">Your Email</label>
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="password" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example4cg">Password</label>
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="password" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                                    </div>
+
+                                    <div class="form-check d-flex justify-content-center mb-5">
+                                        <label class="form-check-label" for="form2Example3g">
+                                            Forgot your password? <a href="#!" class="text-body"><u>Terms of service</u></a>
+                                        </label>
+                                    </div>
+
+                                    <div class="d-flex justify-content-center">
+                                        <button type="button" class="btn btn btn-outline-success btn-lg">Register</button>
+                                    </div>
+
+                                    <p class="text-center text-muted mt-5 mb-0">Already have an account? <a href="#!" class="fw-bold text-body"><u>Login here</u></a></p>
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+    <!-- <section class="vh-100">
             <h1 class="">Register!</h1>
         
-            <form class="mt-10" method="POST" action="/register">
+            <form class="form-outline mb-4" method="POST" action="/register">
                 @csrf
 
                 <div class="mb-6">
@@ -76,6 +136,5 @@
                         >Submit</button>
                 </div>
             </form>
-        </main>
-    </section>
+    </section> -->
 </x-layout>
