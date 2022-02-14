4edf213bd62d64d927f046781fa7b51df621e4a0
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-02-14 12:20:41 +0100

    creating flash component for flash message

---
+++ resources/views/components/flash.blade.php
@@ -0,0 +1,7 @@
+@if (session()->has('success'))
+    <div class="alert alert-primary m-0 alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
