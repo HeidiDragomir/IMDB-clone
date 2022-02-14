6c71dde377336073936037643bdd0f0ef51a3b65
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-02-14 12:15:03 +0100

    adding title, correcting links, auth checks and form for logout and flash message

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>MovieMe</title>
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 </head>
@@ -14,14 +14,14 @@
     <!-- NAVBAR -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Movieme</a>
+            <a class="navbar-brand" href="/">Movieme</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Movies</a>
@@ -29,13 +29,23 @@
                 </ul>
 
                 <div class="d-flex">
-                    <a href="#" class="btn btn-outline-success">Login</a>
-                    <a href="#" class="btn btn-outline-primary ms-2">Signup</a>
+                    @auth <!-- Checks if the user is sign in or a guest -->
+                    <span class="nav-link text-dark text-uppercase me-3">Welcome {{ auth()->user()->name }}!</span>
+                
+                    <form method="POST" action="/logout">
+                    @csrf
+                    <button class="btn btn-outline-danger" type="submit">Log Out</button>
+                    </form>
+                    @else
+                    <a href="/login" class="btn btn-outline-success">Login</a>
+                    <a href="/register" class="btn btn-outline-primary ms-2">Signup</a>
+                    @endauth
                 </div>
             
             </div>
         </div>
     </nav>
+    <x-flash />
     <!-- NAVBAR -->
 
     <main>
