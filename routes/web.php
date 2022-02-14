1234134b7607ea45c970a7b61793f7d9e64929ee
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-02-14 12:17:44 +0100

    routing for register, login, sessions and logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +19,16 @@ Route::get('/', function () {
     return view('movies');
 });
 
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
 Route::get('/movie', function () {
     return view('movie');
 });
+
+// middleware = app/http/middleware. If your logged in you are not able to create and store.
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
+
+Route::get('/login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('/sessions', [SessionsController::class, 'store'])->middleware('guest');
+
+// middleware = app/http/middleware. If your are a guest you are not able to logout.
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
