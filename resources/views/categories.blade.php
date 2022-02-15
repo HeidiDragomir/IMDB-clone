8820b775d23e2eedece33d3d49377aef80a110ec
fbarfvestam <filip.barfvestam@chasacademy.se>
2022-02-15 20:42:50 +0100

    add grid for categories and foreach loop

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,10 +1,13 @@
 <x-layout>
 
-<h1>categories page</h1>
-@foreach($movies as $movie)
-<p>{{ $movie->title }}</p>
-
-
-@endforeach
+    <div class="container my-4">
+        <!--Grid row-->
+        <div class="row">
+            <!--Grid column-->
+            @foreach($movies as $movie)
+            <div class="col-lg-4 col-md-12 mb-4">
+                <img src="{{ $movie->photo_poster }}" class="img-fluid mb-4" alt="">
+            </div>
+            @endforeach
 
 </x-layout>
