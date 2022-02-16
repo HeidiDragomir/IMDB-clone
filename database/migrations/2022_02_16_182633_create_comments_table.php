a4bdaa4a051c2edb521a4ae82f6b7f7da37e6e36
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-02-16 23:06:49 +0100

    adding migration for comments

---
+++ database/migrations/2022_02_16_182633_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            /* $table->foreignId('movie_id')->constrained()->cascadeOnDelete(); */ // if post deletes, the comment deletes in db
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete(); // if user deletes, the comment deletes in db
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
