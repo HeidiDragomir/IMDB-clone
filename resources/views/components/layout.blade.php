eb89c73761fa5b0ea7ab43d7e0fce573dc5643a6
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-02-16 10:20:00 +0100

    adding js to flashmessage for disapearing after 4sek

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,7 @@
     <title>MovieMe</title>
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -62,6 +63,7 @@
     </footer>
     <!-- FOOTER -->
 
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
