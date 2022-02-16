c25d4f2fbfd6b69b0291f49b391489a3aa63c2b0
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-02-16 23:04:31 +0100

    adding route for comments. and changed from id to slug for movie(show) url

--- routes/web.php
+++ routes/web.php
@@ -17,51 +17,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('/', function () {
-    return view('movies');
-}); */
 
-Route::get("/", function (Category $category) {
-    return view("movies", [
-        "movies" => $category::all(),
-    ]);
-});
+Route::get('/', [MovieController::class, 'index'])->name('home');
 
-Route::get("/movie", function () {
-    return view("movie");
-});
+Route::get('/movies/{movie:slug}', [MovieController::class, 'movie']);
+Route::post('/movies/{movie:slug}/comments', [MovieCommentsController::class, 'store']);
 
-Route::get("/", [MovieController::class, "index"]);
 
-// Route::get('/login', function () {
-//     return view('login');
-// });
-
-Route::get("/genre", function () {
-    return view("genre");
-});
 // middleware = app/http/middleware. If your logged in you are not able to create and store.
-Route::get("/register", [RegisterController::class, "create"])->middleware(
-    "guest"
-);
-Route::post("/register", [RegisterController::class, "store"])->middleware(
-    "guest"
-);
+Route::get("/register", [RegisterController::class, "create"])->middleware("guest");
+Route::post("/register", [RegisterController::class, "store"])->middleware("guest");
 
-Route::get("/login", [SessionsController::class, "create"])->middleware(
-    "guest"
-);
-Route::post("/sessions", [SessionsController::class, "store"])->middleware(
-    "guest"
-);
+Route::get("/login", [SessionsController::class, "create"])->middleware("guest");
+
+Route::post("/sessions", [SessionsController::class, "store"])->middleware("guest");
 
 // middleware = app/http/middleware. If your are a guest you are not able to logout.
-Route::post("logout", [SessionsController::class, "destroy"])->middleware(
-    "auth"
-);
+Route::post("logout", [SessionsController::class, "destroy"])->middleware("auth");
 
-Route::get("/categories/{category}", function (Category $category) {
+
+
+
+
+/* Route::get("/categories/{category}", function (Category $category) {
     return view("categories", [
         "movies" => $category->movies,
     ]);
 });
+
+Route::get("/genre", function () {
+    return view("genre");
+}); */
