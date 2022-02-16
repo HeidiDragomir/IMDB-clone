c868b44635db974b395f91529f611bdac1963ed2
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-02-16 23:01:15 +0100

    adding hasMany for movies

--- app/Models/Movies.php
+++ app/Models/Movies.php
@@ -9,8 +9,31 @@ class Movies extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+    protected $with = ['category'];
+
+    public function scopeFilter($query, array $filters) { 
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+            )       
+        );
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn($query) => 
+                $query->where('slug', $category)
+            )
+        );
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
 }
