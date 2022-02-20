8941b5edb2d8063bf45f896c7d48394997b3a7ff
dinler91 <ahmet.dinler@chasacademy.se>
2022-02-20 19:48:06 +0100

    fix typo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -719,7 +719,7 @@ class DatabaseSeeder extends Seeder
         Movie::create([
             'category_id' => $action->id,
             'slug' => 'leon-the-professional',
-            'title' => 'Léon: The Professiona',
+            'title' => 'Léon: The Professional',
             'year' => 1994,
             'body' => 'Léon, the top hit man in New York, has earned a rep as an effective "cleaner". But when his next-door neighbors are wiped out by a loose-cannon DEA agent, he becomes the unwilling custodian of 12-year-old Mathilda. Before long, Mathilda\'s thoughts turn to revenge, and she considers following in Léon\'s footsteps.',
             'photo_poster' => 'https://www.themoviedb.org/t/p/w1280/yI6X2cCM5YPJtxMhUd3dPGqDAhw.jpg',
