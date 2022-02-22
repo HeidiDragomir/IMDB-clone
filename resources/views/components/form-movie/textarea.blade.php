5e9fc0e8bc4b083a2a680023529132948bd90925
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-02-22 19:55:08 +0100

    delete file

--- resources/views/components/form-movie/textarea.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['name'])
-
-<div class="form-outline mb-4">
-    <label class="form-label" for="{{ $name }}">{{ ucwords($name) }}</label>
-    <textarea class="form-control form-control-lg" name="{{ $name }}" id="{{ $name }}" cols="30" rows="7" required>{{ old($name) }}</textarea>
-
-    <x-form-movie.error name='{{ $name }}' />
-
-</div>
