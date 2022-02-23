713a7a6447f0c1956b917f56cd6fc72e6bfe34a4
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-02-23 08:49:54 +0100

    update file to keep the old values in form

--- resources/views/components/form-movie/input.blade.php
+++ resources/views/components/form-movie/input.blade.php
@@ -3,7 +3,7 @@
 <div class="form-outline mb-4">
     <label class="form-label" for="{{ $name }}">{{ ucwords($name) }}</label>
 
-    <input class="form-control form-control-lg" type="text" name="{{ $name }}" id="{{ $name }}" required />
+    <input class="form-control form-control-lg" type="text" name="{{ $name }}" id="{{ $name }}" {{ $attributes(['value' => old($name)]) }} />
 
     <x-form-movie.error name='{{ $name }}' />
 
