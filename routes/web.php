0d24ef3c752f1fa583735e1073f2e8eff3e2caa1
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-02-24 17:59:03 +0100

    add all the Routes for comments approving

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCommentController;
 use App\Http\Controllers\AdminMovieController;
 use App\Http\Controllers\MovieCommentsController;
 use App\Http\Controllers\MovieController;
@@ -23,7 +24,23 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", [MovieController::class, "index"])->name("home");
 
 Route::get("/movies/{movie:slug}", [MovieController::class, "show"]);
+
+
+// Comments
 Route::post("/movies/{movie:slug}/comments", [MovieCommentsController::class, "store",]);
+
+
+Route::get("/admin/dashboard/comments", [AdminCommentController::class, "index",])->middleware("admin");
+
+
+Route::post("/admin/dashboard/comments", [AdminCommentController::class, "store"])->middleware("admin");
+
+
+Route::post("/admin/dashboard/comments/approve", [AdminCommentController::class, "approval"])->middleware("admin");
+
+
+Route::delete("/admin/dashboard/comments/{comment}", [AdminCommentController::class, "destroy"])->middleware("admin");
+
 
 // middleware = app/http/middleware. If your logged in you are not able to create and store.
 Route::get("/register", [RegisterController::class, "create"])->middleware("guest");
@@ -38,7 +55,7 @@ Route::post("logout", [SessionsController::class, "destroy"])->middleware("auth"
 
 // Watchlist
 Route::get("/movies/watchlist", [WatchlistController::class, "index"]);
-Route::post("/movie/{movie:slug}/watchlist", [WatchlistController::class,"store",]);
+Route::post("/movie/{movie:slug}/watchlist", [WatchlistController::class, "store",]);
 
 // Admin
 Route::get('/admin/dashboard', [SessionsController::class, "authAdmin"])->middleware('admin', 'auth');
