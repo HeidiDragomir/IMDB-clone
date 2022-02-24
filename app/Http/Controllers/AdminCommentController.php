efb5f4c1c3f07664bcd48573d59dc275ffa6a6fa
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-02-24 17:59:46 +0100

    create a new controller for approving

---
+++ app/Http/Controllers/AdminCommentController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+
+class AdminCommentController extends Controller
+{
+    public function index()
+    {
+        return view('admin.approvalComments', [
+            'comments' => Comment::all()
+        ]);
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'stars' => 'required',
+            'body' => 'required',
+            'approved' => 'required',
+            'movie_id' => ['required', Rule::exists('movies', 'id')],
+            'user_id' => ['required', Rule::exists('user', 'id')]
+        ]);
+
+        Comment::create($attributes);
+
+        return redirect('/');
+    }
+
+
+    public function approval(Request $request)
+    {
+
+        $comment = Comment::find($request->commentId);
+        $approveVal = $request->approved;
+        if ($approveVal == 'on') {
+            $approveVal = 1;
+        } else {
+            $approveVal = 0;
+        }
+
+        $comment->approved = $approveVal;
+        $comment->save();
+
+        return back();
+    }
+
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+        return back()->with('success', 'Comment Deleted!');
+    }
+}
