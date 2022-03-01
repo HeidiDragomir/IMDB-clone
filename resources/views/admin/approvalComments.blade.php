68a8bfb15732de987dd17465929d215ce77fa3d8
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-03-01 18:59:22 +0100

    change size table and make it responsive

--- resources/views/admin/approvalComments.blade.php
+++ resources/views/admin/approvalComments.blade.php
@@ -4,84 +4,90 @@
     <section>
         <h1 class="mb-4 mt-5 pb-2 pb-md-0 mb-md-5 text-center">Manage Comments</h1>
 
-        <div class="card-body m-5 p-5">
-
-            <table class="table table-striped custab">
-                <thead>
-                    <tr>
-                        <th>
-                            User
-                        </th>
-                        <th>
-                            Movie
-                        </th>
-                        <th>
-                            Comment
-                        </th>
-                        <th>
-                            Created at
-                        </th>
-                        <th>
-                            Status
-                        </th>
-                        <th colspan="2" class="text-center">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($comments as $comment)
-                    <tr>
-                        <td class="align-middle">
-                            {{ $comment->user->username}}
-
-                        </td>
-                        <td class="align-middle">
-                            {{ $comment->movie->title }}
-
-                        </td>
-
-                        <td class="align-middle">
-                            {{ $comment->body }}
-
-                        </td>
-                        <td class="align-middle">
-                            {{ $comment->created_at }}
-                        </td>
-                        @if ($comment->approved == true)
-                        <td class="align-middle">
-                            Approved
-                        </td>
-                        @else
-                        <td class="align-middle">
-                            Waiting
-                        </td>
-                        @endif
-                        <td class="align-middle">
-
-                            <form class="form-check form-check-inline" action="/admin/dashboard/comments/{{ $comment->id }}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <input class="btn btn-success" type="submit" value="Approve">
+        <div class="pt-5 pb-5">
+            <div class="container-xl">
+                <div class="row d-flex table-responsive-lg justify-content-center h-100">
 
 
-                            </form>
-                        </td>
-                        <td class="align-middle">
+                    <table class="table w-auto table-striped custab">
+                        <thead>
+                            <tr>
+                                <th>
+                                    User
+                                </th>
+                                <th>
+                                    Movie
+                                </th>
+                                <th>
+                                    Comment
+                                </th>
+                                <th>
+                                    Created at
+                                </th>
+                                <th>
+                                    Status
+                                </th>
+                                <th colspan="2" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($comments as $comment)
+                            <tr>
+                                <td class="align-middle">
+                                    {{ $comment->user->username}}
 
-                            <form method="POST" action="/admin/dashboard/comments/{{ $comment->id }}">
-                                @csrf
-                                @method('DELETE')
+                                </td>
+                                <td class="align-middle">
+                                    {{ $comment->movie->title }}
 
-                                <input class="btn btn-danger" type="submit" value="Delete">
-                            </form>
-                        </td>
+                                </td>
 
-                    </tr>
+                                <td class="align-middle">
+                                    {{ $comment->body }}
 
-                    @endforeach
-                </tbody>
+                                </td>
+                                <td class="align-middle">
+                                    {{ $comment->created_at }}
+                                </td>
+                                @if ($comment->approved == true)
+                                <td class="align-middle">
+                                    Approved
+                                </td>
+                                @else
+                                <td class="align-middle">
+                                    Waiting
+                                </td>
+                                @endif
+                                <td class="align-middle">
 
-            </table>
+                                    <form class="form-check form-check-inline" action="/admin/dashboard/comments/{{ $comment->id }}" method="POST">
+                                        @csrf
+                                        @method('PATCH')
+                                        <input class="btn btn-success" type="submit" value="Approve">
+
+
+                                    </form>
+                                </td>
+                                <td class="align-middle">
+
+                                    <form method="POST" action="/admin/dashboard/comments/{{ $comment->id }}">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <input class="btn btn-danger" type="submit" value="Delete">
+                                    </form>
+                                </td>
+
+                            </tr>
+
+                            @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
         </div>
+
 
     </section>
 </x-layout>
