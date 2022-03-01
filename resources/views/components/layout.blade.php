604c22e6cb01022e74ed7ea4ea6cb3eb5b531c1c
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-03-01 19:46:01 +0100

    fix review section responsive

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>MovieMe</title>
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     <link rel="shortcut icon" href="favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -54,8 +53,8 @@
                         </ul>
                     </div>
                     @else
-                    <a href="/login" class="btn btn-outline-primary">Login</a>
-                    <a href="/register" class="btn btn-primary ms-2">Signup</a>
+                    <a href="/login" class="btn btn-outline-dark">Login</a>
+                    <a href="/register" class="btn btn-dark ms-2">Signup</a>
                     @endauth
                 </div>
             </div>
