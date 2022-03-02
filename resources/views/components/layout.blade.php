3fb67b3b68f1f527f77cfb9bc4534c4098de903d
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-03-02 12:18:07 +0100

    add meta data in head

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="Movieme is a source for movie content. Find ratings and reviews for the newest movies.">
+    <meta name="keywords" content="Movieme, Movie, Movies, rating, reviews">
+    <meta name="author" content="Chas Academy Students">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>MovieMe</title>
     <link rel="shortcut icon" href="favicon.ico">
