e1b09d41d671b0d5d32cb727119cbe9d5e8c8cda
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-03-05 11:56:16 +0100

    migration for lists table

--- database/migrations/2022_03_04_120849_create_mlists_table.php
+++ database/migrations/2022_03_04_120849_create_mlists_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('mlists', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->unsignedBigInteger('user_id')->onDelete('cascade');
             $table->string('title');
             $table->timestamps();
